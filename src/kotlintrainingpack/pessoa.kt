3d635dff156d21bfe4fe7e7c754e84c17906b9a0
GitHoudini <vitorbecker09@gmail.com>
2021-03-29 14:03:54 -0300

    Private set member

--- src/kotlintrainingpack/pessoa.kt
+++ src/kotlintrainingpack/pessoa.kt
@@ -1,17 +1,24 @@
 package kotlintrainingpack
 
 class Pessoa {
-    var nome:String = "João"
+    var nome:String = "Joao"
     var cpf:String = "123.123.123-12"
+    private set
 
     inner class Endereco {
         var rua:String = "Teste, numero 8"
     }
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"    
 }
 
 fun main() {
     val bloco = Pessoa()
+
     println(bloco.nome)
     println(bloco.cpf)
     println(bloco.Endereco().rua)
+    println(bloco.pessoaInfo())
 }
